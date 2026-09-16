27f6ce67022b2be1748ce210f233eb42dc4b4417
Nahuel Pages Oviedo <127339949+GuyaSuna@users.noreply.github.com>
2026-09-16 19:59:19 -0300

    init

---
+++ Ej1-Semana3/Models/Turno.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ej1_Semana3.Models
+{
+    public class Turno
+    {
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Nombre es obligatorio")]
+        public string NombreMascota { get; set; } = string.Empty;
+        [Required(ErrorMessage ="Responsable es obligatorio")]
+        public string Responsable {  get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Servicio contratado es obligatorio")]
+        public string ServicioSolicitado {  get; set; } = string.Empty;
+
+        public bool Confirmado { get; set; }
+    }
+}
---
+++ Ej1-Semana3/Services/TurnoService.cs
@@ -0,0 +1,48 @@
+﻿using Ej1_Semana3.Models;
+
+namespace Ej1_Semana3.Services
+{
+    public class TurnoService
+    {
+
+        private readonly List<Turno> _listaTurnos = new();
+
+
+        public List<Turno> GetAllTurnos()
+        {
+            return _listaTurnos;
+        }
+        public Turno? GetTurno(int id)
+        {
+            return _listaTurnos.FirstOrDefault(t => t.Id == id);
+        }
+        public void AgregarTurno(Turno turno)
+        {
+            int nuevoId = _listaTurnos.Count == 0 ? 1 : _listaTurnos.Max(turno => turno.Id) +1;
+
+            turno.Id = nuevoId;
+
+            _listaTurnos.Add(turno);
+        }
+
+        public bool Eliminar(int id)
+        {
+            var t = GetTurno(id);
+
+            if(t == null) return false;
+
+            _listaTurnos.Remove(t);
+            return true;
+
+        }
+
+        public bool Modificar(Turno turno)
+        {
+            var t = GetTurno(turno.Id);
+
+            Turno nuevoTurno = new() { }
+        }
+
+
+    }
+}
